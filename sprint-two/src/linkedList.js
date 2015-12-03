b9f001f94e2b96c41a02aaffa30afc1bbba3b5cf
TGAStudent <tga.student@telegraphacademy.com>
2015-12-03 12:31:56 -0800

    in progress sprint 2

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,10 +3,28 @@ var LinkedList = function(){
   list.head = null;
   list.tail = null;
 
+
+
   list.addToTail = function(value){
+    //set value of node.next of current node to 
+      //debugger;
+      if(list.tail === null){
+        list.head = new Node(value);
+        list.tail = list.head;
+      } else{
+
+      var currentNode = list.tail;
+      list.head.next = currentNode;
+      list.tail = new Node(value);
+      }
   };
 
   list.removeHead = function(){
+    //Protect against negative values (size of list never below zero)
+    var nextNode = list.head.next
+    list.head = undefined;
+    list.head = nextNode;    
+
   };
 
   list.contains = function(target){
